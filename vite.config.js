0ad0fbe25bcca94428009e3110a0a8ba176e074a
AtharvPS13 <fake34811289@gmail.com>
2025-06-22 20:01:08 +0530

    deployment changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Password_Generator/',
+  base: "/Password_Generator",
   plugins: [
     react(),
     tailwindcss(),
